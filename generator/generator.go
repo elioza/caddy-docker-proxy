f7e2c02bf37e1ff8659ed3554312fadb95df05fd
elioza <elioza@users.noreply.github.com>
2022-08-25 12:26:17 +0200

    Update generator.go

--- generator/generator.go
+++ generator/generator.go
@@ -11,9 +11,9 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/swarm"
-	"github.com/lucaslorentz/caddy-docker-proxy/v2/caddyfile"
-	"github.com/lucaslorentz/caddy-docker-proxy/v2/config"
-	"github.com/lucaslorentz/caddy-docker-proxy/v2/docker"
+	"github.com/elioza/caddy-docker-proxy/v2/caddyfile"
+	"github.com/elioza/caddy-docker-proxy/v2/config"
+	"github.com/elioza/caddy-docker-proxy/v2/docker"
 
 	"go.uber.org/zap"
 )
